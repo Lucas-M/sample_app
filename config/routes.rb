386a7cb049a31362ce9aa76d5dd6bbae8df4ddca
Lucas-M <lucas.macd@gmail.com>
2014-02-02 20:29:33 -0500

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
   # Original method of routes
   # get "static_pages/home"
   # get "static_pages/help"
@@ -8,6 +9,7 @@ SampleApp::Application.routes.draw do
   # New method of dynamic routes
   
   root  'static_pages#home'
+  match '/signup',  to: 'users#new',            via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
